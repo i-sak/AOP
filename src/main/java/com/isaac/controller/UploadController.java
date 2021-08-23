1c27e88c0c3a5183adf5c165dfd8e59796fd0c40
kimisak <isak7263@gmail.com>
2021-08-23 23:15:09 +0900

    ajax 파일 업로드

--- src/main/java/com/isaac/controller/UploadController.java
+++ src/main/java/com/isaac/controller/UploadController.java
@@ -34,4 +34,25 @@ public class UploadController {
 			} // end catch
 		} // end for
 	}
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		log.info("upload ajax");
+	}
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		log.info("upload ajax post.............");
+		String uploadFolder = "C:\\storage";
+		for(MultipartFile multipartFile : uploadFile) {
+			log.info("---------------------------------------");
+			log.info("Upload File Name : "+multipartFile.getOriginalFilename());
+			log.info("Upload File Size : "+multipartFile.getSize());
+			
+			File savefile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			try {
+				multipartFile.transferTo(savefile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			} // end catch
+		} // end for
+	}
 }
