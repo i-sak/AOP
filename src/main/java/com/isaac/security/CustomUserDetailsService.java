eb28443373b3f636816015c6102da6c1c08db56d
kimisak <isak7263@gmail.com>
2021-10-03 13:00:24 +0900

    스프링 시큐리티 커스텀 테이블을 사용하기 위한 세팅 적용

---
+++ src/main/java/com/isaac/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.isaac.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.isaac.domain.MemberVO;
+import com.isaac.mapper.MemberMapper;
+import com.isaac.security.domain.CustomUser;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.warn("Load User By UserName : " + username);
+		
+		// userName means userid
+		MemberVO vo = memberMapper.read(username);
+		
+		log.warn("queried by member mapper : " + vo);
+		
+		return vo == null ? null : new CustomUser(vo);
+	}
+}
